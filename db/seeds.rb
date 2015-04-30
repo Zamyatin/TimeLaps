26a9f9b650636b26b9815ff406d887ae0e7a3bb5
Raj Singh <raj@foreversingh.com>
2015-04-30 02:06:07 -0500

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([{email: 'raj@foreversingh.com', password: 'testPass', username: "Zamyatin"}, {email:'beth@gmail.com', password: 'password', username: "Cynaria"}])
+
+garages = Garage.create([{user_id: users.first, name: 'My Sweet Track Stuff'},
+  {user_id: users.last, name: "My Urban Battle Wagons"}])
+
+Car.create(make:"Honda", model:"S2000", color: 'red', owner_id: users.first)
+Car.create(make:"Honda", model:'Civic', color: 'avocado', owner_id: users.last, trim:'Type-R')
+Car.create(make:"Scion", model:'FR-S', color:'Firestorm', owner_id: users.first)
