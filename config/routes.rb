64c24a6b4f6f2a3b767ee3c413fac3b90564e640
Raj Singh <raj@foreversingh.com>
2015-05-27 01:48:10 -0500

    cleaning up style

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'visitors#index'
   resources :users, only: [:show, :index]
   # Example of regular route:
-    # get 'products/:id' => 'catalog#view'
+  # get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
