20531387248935d99a1eb058787f96af5e1f1f99
Raj Singh <raj@foreversingh.com>
2015-05-27 01:40:22 -0500

    added data to users model

---
+++ db/migrate/20150527034703_add_info_to_users.rb
@@ -0,0 +1,8 @@
+class AddInfoToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_index :users, :first_name
+    add_column :users, :last_name, :string
+    add_index :users, :last_name
+  end
+end
